f5380f63204dd001a14949899833266f0d61573b
samara <126591429+sanzinha@users.noreply.github.com>
2025-05-06 09:03:47 -0300

    Add files via upload

---
+++ jogos/forca.py
@@ -0,0 +1,24 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_enforca = "cupuaçu"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        chute = input ("qual é a letra?")
+        posicao = 0
+        for letra in palavra_enforca:
+            if(chute == letra):
+                print(f'encontrei a letra {letra} na posiçao{posicao}')
+                posicao = posicao + 1
+
+        print ("jogando...")  
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
